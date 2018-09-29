d1cda895fad0436847a036a1ec0274523ba8e879
GiblerW <westongibler@gmail.com>
2018-09-29 15:55:29 -0600

    Initial Commit

---
+++ Treez/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Treez
+//
+//  Created by Weston Gibler on 9/29/18.
+//  Copyright © 2018 Weston Gibler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
